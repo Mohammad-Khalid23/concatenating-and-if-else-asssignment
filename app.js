dc024f131dfcff39d8e74785bef4e4798deac10b
Mohammad Khalid <m.k.ayub23@gmail.com>
2017-04-19 21:42:36 +0500

    lab assignment of if else

---
+++ app.js
@@ -0,0 +1,600 @@
+/********Mohammad Khalid , Batch 4.1 , Section:Orange ********/
+
+
+/*************Task 1*******/
+// var name=prompt("Please Enter Your Name", "Name");
+
+// alert("Assalam U Alykum! "+ name);
+
+
+
+
+
+/*************Task 2*******/
+
+// var num=+prompt("Enter Number", "2");
+// var inc=0;
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+// console.log( num+" x "+ ++inc +" = "+ num* inc);
+
+
+
+
+
+/*************Task 3*******/
+// var name = prompt("Please Enter Your City", "City");
+// if (name === "karachi") {
+//     alert("Welcome to city of Light");
+// } else {
+//     alert("You did not enter karachi")
+// }
+
+
+
+
+/*************Task 4*******/
+// var gender = prompt("Please Enter Your Gender", "male");
+// if (gender === "male") {
+//     alert("Good Morning Sir Jee!");
+// } else if (gender === "female") {
+//     alert("Good Morning Madam Jee!")
+// } else {
+//     alert("Sorry!!! You Have Gender Problem")
+// }
+
+
+
+
+
+/*************Task 5*******/
+
+// var signalColor = prompt("Enter The Color of Signal", "Green");
+// var message;
+// if (signalColor === "green") {
+//     alert(message = "vehicles can move now");
+// } else if (signalColor === "yellow") {
+//     alert(message = "Vehicles should get ready to move");
+// } else if (signalColor === "red") {
+//     alert(message = "vehicles Must Stop");
+// } else {
+//     alert("Sabar Karen Allah! Pak Ajar Dengey :)")
+// }
+
+
+
+
+
+/*************Task 6*******/
+
+// var maxAge=+prompt("Enter max Age", "30");
+// var currentAge=+prompt("Enter max Age", "20");
+
+// if(maxAge===currentAge){
+//     alert("You are Welcome My Brother")
+// }else{
+//     alert(" Sorry Your are not in the age Limit")
+// }
+
+
+
+
+
+/*************Task 7*******/
+
+// var remainnigFuel = +prompt("Enter Your remainnig Fuel", "1")
+
+// if(remainnigFuel<0.25){
+//     console.log("Please refill the fuel in your car")
+// } else{
+//     console.log("Enjoy Your Ride")
+// }
+
+
+
+
+
+
+/*************Task 8*******/
+
+// /*************(a)*******/
+// var a = 4;
+// if (++a === 5) {
+//     alert("given condition for variable a is true");
+//     console.log("Result  display for Variable (a)");
+
+// } else {
+//     console.log("Result Not display for Variable (a)");
+// }
+
+
+// /*************(b)*******/
+
+
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+
+//     console.log("Result  display for Variable (b)");
+
+// } else {
+//     console.log("Result Not display for Variable (b)");
+// }
+
+// /*************(c)************/
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+
+//     console.log("Result of condition 1 for (C) displayed");
+// } else {
+//     console.log("Result of condition 1 for (C) not displayed");
+
+// }
+// if (c === 13) {
+//     alert("condition 2 is true");
+//     console.log("Result of condition 2 for (C) displayed");
+
+// } else {
+//     console.log("Result of condition 2 for (C) not displayed");
+
+// }
+// if (++c < 14) {
+//     alert("condition 3 is true");
+//     console.log("Result of condition 3 for (C) displayed");
+
+// } else {
+//     console.log("Result of condition 3 for (C) not displayed");
+
+// }
+// if (c === 14) {
+//     alert("condition 4 is true");
+//     console.log("Result of condition 4 for (C) displayed");
+
+// } else {
+//     console.log("Result of condition 4 for (C) not displayed");
+
+// }
+
+// /*************(d)************/
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//     alert("The cost equals");
+//     console.log("Result  display for (d) ");
+// } else {
+//     console.log("Result  not display for (d) ");
+
+// }
+
+// /*************(e)************/
+// if (true) {
+//     alert("True");
+//     console.log("Result TRUE  display for (e) ");
+
+// }
+// if (false) {
+//     alert("False");
+//     console.log("Result FALSE  display for (e) ");
+// }
+
+
+// /*************(f)************/
+
+// if ("car" < "cat") {
+//     alert("car is smaller than cat");
+//     console.log("Result   displayed  for (f) ");
+// } else {
+//     console.log("Result  not displayed  for (f) ");
+// }
+
+
+
+
+
+
+/*************Task 9*******/
+
+// var sub1 = +prompt("Enter Marks of Math", 20);
+// if (sub1 <= 100) {
+//     var sub2 = +prompt("Enter Marks of Physics", 20);
+//     if (sub2 <= 100) {
+//         var sub3 = +prompt("Enter Marks of Chemistry", 20);
+//         if (sub3 <= 100) {
+
+//             var obtainedMarks = sub1 + sub2 + sub3;
+//             var totalMarks = 300;
+//             var grade;
+//             var remarks;
+
+//             percentage = (obtainedMarks / totalMarks) * 100;
+
+//             if (percentage >= 80 && percentage <= 100) {
+//                 grade = "A-one"
+//                 remarks = "Excellent"
+
+//             } else if (percentage >= 70 && percentage < 80) {
+//                 grade = "A"
+//                 remarks = "Good"
+//             } else if (percentage >= 60 && percentage < 70) {
+//                 grade = "B"
+//                 remarks = "You need to improve"
+//             } else if (percentage < 60) {
+//                 grade = "Sorry you are fail"
+//                 remarks = "Do Hard Work and Try Again"
+//             } else {
+//                 console.log("Invalid Input");
+
+//             }
+//             document.write("<br/>" + "Total Marks : " + totalMarks)
+//             document.write("<br/>" + "Marks Obtained : " + obtainedMarks)
+//             document.write("<br/>" + "Percentage : " + percentage.toFixed(2))
+//             document.write("<br/>" + "Grade : " + grade)
+//             document.write("<br/>" + "Remarks : " + remarks)
+
+//         }// 3rd cond
+//         else {
+//             document.write("Please Make Sure your Chemistry Number is in 100");
+//         }
+
+//     } //2nd Cond
+//     else {
+//         document.write("Please Make Sure your Physics Number is in 100");
+//     }
+
+// } //1st cond
+// else {
+//     document.write("Please Make Sure your Math Number is in 100");
+// }
+
+
+
+
+
+/*************Task 10*******/
+
+// var nameItem1 = prompt("Enter the Name of 1st Item", "Default");
+// var nameItem2 = prompt("Enter the Name of 2st Item", "Default");
+// var item1Price = +prompt("Enter the Price of " + nameItem1, 100);
+// var item2Price = +prompt("Enter the Price of " + nameItem2, 100);
+// var item1Quantity = +prompt("How Many " + nameItem1 + " you took?", 1);
+// var item2Quantity = +prompt("How Many " + nameItem2 + " you took?", 1);
+// var shippingCharges = +prompt("Please Enter the Shipping Charges", 10);
+
+
+// // var item1Cost= item1Price* item1Quantity;
+// // var item2Cost= item2Price* item2Quantity;
+// // var totalCost=item1Cost+item2Cost;
+// var totalCost = (item1Price * item1Quantity) + (item2Price * item2Quantity);
+
+// if (totalCost > 2000) {
+//     discount = (0.10) * totalCost;
+// }
+
+// document.write("<h1>"+ "Shopping Cart"+"<h1/>")
+
+// document.write("<br/>" + "Price of " + nameItem1 + " is " + item1Price);
+// document.write("<br/>" + "Quantity of " + nameItem1 + " is " + item1Quantity);
+
+// document.write("<br/>" + "Price of " + nameItem1 + " is " + item1Price);
+// document.write("<br/>" + "Quantity of " + nameItem2 + " is " + item2Quantity);
+
+// document.write("<br/>")
+// document.write("<br/>" + "Shipping Charges " + shippingCharges);
+// document.write("<br/>")
+
+// document.write("<br/>" + "The Total Cost of your order is " + totalCost);
+// document.write("<br/>" + "Discounted Price is " + (totalCost - discount));
+
+
+
+/*************Guess Games*******/
+
+/*************Task 11*******/
+
+// var secretNumber = 5;
+
+// var guessNumber = +prompt("Hey!! Buddy Guess the Number", 1)
+
+// if (guessNumber === secretNumber) {
+//     document.write("Bingo! Correct answer")
+// } else if (secretNumber === (guessNumber + 1)) {
+
+//     document.write("Close enough to the correct answer")
+// }else {
+//     document.write("Sorry Buddy!! try again")
+// }
+
+
+
+
+
+
+/*************Task 12*******/
+
+
+// var number=+prompt("Enter the Number")
+
+// if((number%3)===0){
+//     document.write(number+" is divisible by 3")
+// }else{
+//     document.write(number+" is not divisible by 3")
+
+// }
+
+
+
+
+
+
+/*************Task 13*******/
+
+// var team1 = prompt("Enter the Name of 1st Team");
+// var scoreOfTeam1 = +prompt("Enter the Score of " + team1, 100);
+
+// var team2 = prompt("Enter the Name of 2st Team");
+// var scoreOfTeam2 = +prompt("Enter the Score of " + team2, 100);
+
+// if(scoreOfTeam1===scoreOfTeam2){
+//     document.write("Ye Match tie Hogya Hai")
+// }else if(scoreOfTeam1>scoreOfTeam2){
+//     document.write(team1+" aWon the Match")
+//     }else{
+//         document.write(team2+" Won the Match")
+//     }
+
+
+
+
+/*************Task 14*******/
+
+// var num=10;
+// var string="khalid Ayub"
+// var bool=true;
+
+// alert( num+" is "+typeof(num))
+// alert( string+" is "+typeof(string))
+// alert( bool+" is "+typeof(bool))
+
+
+
+/*************Task 15*******/
+
+// var number=+prompt("Enter the Number")
+
+// if((number%2)===0){
+//     document.write(number+" is Even Number")
+// }else{
+//     document.write(number+" is Odd Number")
+// }
+
+
+
+
+/*************Task 16*******/
+
+// var temp = +prompt("Enter the Temperature of Karachi", 25)
+// if (temp > 40 && temp <= 60) {
+//     document.write("It is too hot outside.")
+// }
+// else if (temp > 30 && temp <= 40) {
+//     document.write("The Weather today is Normal.")
+// } else if (temp > 20 && temp <= 30) {
+//     document.write("Today’s Weather is cool.")
+
+// } else if (temp > 10 && temp <= 20) {
+//         document.write("“OMG! Today’s weather is so Cool.")
+
+// } else {
+//     document.write("Please! make you entered right temperature")
+// }
+
+
+
+
+/*************Task 17*******/
+
+// var num1 = +prompt("Enter 1st Number");
+
+// var num2 = +prompt("Enter 2nd Number");
+
+// var operator = prompt("Enter Operator", "+");
+
+// if (operator === "+") {
+//     alert(num1 + operator + num2 + " = " + (num1 + num2))
+
+
+// } else if (operator === "-") {
+//     alert(num1 + operator + num2 + " = " + (num1 - num2))
+
+
+// } else if (operator === "*") {
+//     alert(num1 + operator + num2 + " = " + (num1 * num2))
+
+// } else if (operator === "/") {
+//     alert(num1 + operator + num2 + " = " + (num1 / num2) )
+// } 
+// else if (operator === "%") {
+//         alert(num1 + operator + num2 + " = " + (num1 % num2) )
+// }
+// else {
+//     alert(operator+" is not Operator")
+// }
+
+
+
+
+/*************Task 18*******/
+// var day = prompt("Please Enter What's Today", "monday");
+
+// if ((day === "monday") || (day === "tuesday") || (day === "wednesday") || (day === "thursday") || (day === "friday")) {
+
+//     alert("It's a Week Day!!");
+// } else if (day === "saturday") {
+//     alert("It's a Weekend!!")
+
+// }else if(day === "sunday"){
+//     alert("Yay!! It's Holiday")
+// } else{
+//     alert(day+" is not a day , Please Enter a Day")
+// }
+
+
+
+/*************Task 19*******/
+//  var score=prompt("Enter your Score", 50);
+//   if(score > 50){
+
+//       alert(" You are Passed")
+//   } else{
+//           alert(" Try Again")
+
+//   }
+
+
+
+
+/*************Task 20*******/
+
+// var num1 = +prompt("Enter 1st number", 10);
+// var num2 = +prompt("Enter 2nd number", 5);
+
+// if (num1 > num2) {
+
+//     alert("The Greater Number of " + num1 + " and " + num2 + " is " + num1)
+// } else if (num1 < num2) {
+//     alert("The Greater Number of " + num1 + " and " + num2 + " is " + num2)
+
+// } else if (num1 === num2) {
+//         alert("The Number of " + num1 + " and " + num2 + " are equal ")
+
+// }else {
+//     alert("Please Enter Numbers")
+// }
+
+
+
+
+/*************The Translator*******/
+
+
+/*************Task 21*******/
+
+// var languageCode = prompt("Enter the language code, from english, spanish ,french", "en")
+
+// if (languageCode === "en") {
+//     alert("Hello!! World");
+// }
+// else if (languageCode === "sp") {
+//     alert("Hola Mundo");
+// }
+// else if (languageCode === "fr") {
+//     alert("Bonjour le monde");
+// } else {
+//     alert("Enter Valid Language Code")
+// }
+
+
+
+/*************Task 22*******/
+// var num = +prompt("Enter a number to check Positive or Negative", 1);
+
+// if (num > 0) {
+//     alert(num + " is positive number");
+// } else if (num < 0) {
+//     alert(num + " is negative number");
+
+// } else if (num < 0) {
+//     alert(num + " is not postive or negative");
+
+// }
+// else {
+//     alert(num);
+
+// }
+
+
+
+
+/*************Task 23*******/
+
+// var noun = prompt("Enter a noun", "cat");
+// var num = +prompt("Enter a number", 1);
+// if (num === 1) {
+//     alert(noun);
+// } 
+// else if (num > 1) {
+//     alert(num + " " + noun + "s");
+
+// } 
+//     else if (num === 0 || num < 1) {
+//         alert("Please Enter Number Greater Than Zero")
+// }
+//      else {
+//         alert("Please Enter a number")
+// }
+
+
+
+
+
+
+
+
+// var myFirstName=prompt("Your First Name", "Khalid");
+// var myLastName=prompt("Your Last Name", "Ayub");
+// if(myFirstName==="Khalid" && myLastName==="Ayub"){
+//     console.log("Apka Jawab Durust hai")
+// }
+// else{
+// console.log("Apka Jawab Ghalat hai")
+
+// }
+
+
+// var totalNumber=+prompt("Enter Your Grade","60");
+
+// if (totalNumber >= 60 && totalNumber < 70) {
+//     console.log("Your Got B Grade");
+// } else if (totalNumber >= 70 && totalNumber < 80) {
+//     console.log("Your Got A Grade");
+// } else if (totalNumber >= 80 && totalNumber <= 100) {
+//     console.log("Your Got A+1 Grade");
+// } else if (totalNumber < 60) {
+//     console.log("Sorry you are Fail :( ");
+// }else{
+//     console.log("Kiya Likh Rahe Ho Bhai Number Likho na!!");
+
+// }
+
+
+// var userName = prompt("Enter Your name", "Khalid")
+// var email = prompt("Enter Your Email", "khalid123@gmail.com")
+// var result;
+// if (userName === "khalid" && email === "khalid123@gmail.com") {
+//     var totalNumber = +prompt("Enter Your Grade", "60");
+
+//     if (totalNumber >= 60 && totalNumber < 70) {
+//         console.log(result = "Your Got B Grade");
+//     } else if (totalNumber >= 70 && totalNumber < 80) {
+//         console.log(result = "Your Got A Grade");
+//     } else if (totalNumber >= 80 && totalNumber <= 100) {
+//         console.log(result = "Your Got A+1 Grade");
+//     } else if (totalNumber < 60) {
+//         console.log(result = "Sorry you are Fail :( ");
+//     } else {
+//         console.log("Kiya Likh Rahe Ho Bhai Number Likho na!!");
+//     }
+// } else {
+//     console.log("Invalid userName and Password");
+// }
